f79dc98409d32ed6500f04f89424ab91933c206f
Shaikh_Sohel <93519202+shaikhsohel100100@users.noreply.github.com>
2024-10-10 16:55:02 +0530

    Delete Portfolio directory

--- Portfolio/main.js
+++
@@ -1,9 +0,0 @@
-
-//  ========Navigation bar effects on Scroll========
-
-window.addEventListener("scroll", function(){
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky",window.scrollY > 0);
-});
-
-
